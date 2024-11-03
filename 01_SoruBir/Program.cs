c034fa1bc3a9ad691b8f9b32dded1f824561f078
Muhammed Kizilirmak <senel_1996@hotmail.com>
2024-11-03 22:58:06 +0300

    Add project files.

---
+++ 01_SoruBir/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _01_SoruBir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // İki sayının toplamını veren uygulamayı yazınız.
+
+            #region Çözüm 1
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine(sonuc);
+
+            // Daha kısa tanımlaması ve sonuc değişkenini kullanmadan ekrana yazdırma işlemi.
+            int sayi3 = 10, sayi4 = 20;
+            Console.WriteLine(sayi3 + sayi4);
+            #endregion // Çözüm 1
+
+            #region Çözüm 2
+            Console.Write("Lütfen sayı1 değerini giriniz: ");
+            // Kullanıcıdan alınan tüm değerler string yani metinsel değer olarak gelecektir.
+            // Değeri hangi işlem tipine dair kullanılacaksa o tipe çevirme işlemi yapılmalıdır.
+            #region String Tipi Int Tipine Çevirme
+
+            #region Convert İle Çevirme
+            int sayi5 = Convert.ToInt32(Console.ReadLine());
+            #endregion // Convert İle Çevirme
+
+            #region Parse Methodu İle Çevirme
+            // yanında verilen tipe çevirilir.
+            Console.Write("Lütfen sayı2 değerini giriniz: ");
+            int sayi6 = int.Parse(Console.ReadLine());
+            #endregion // Parse Methodu İle Çevirme
+
+            #endregion // String Tipi Int Tipine Çevirme
+
+            Console.WriteLine(sayi5 + sayi6);
+
+            // Console.ReadLine programı bekletir, bir klavyeden değer beklenmektedir.
+            #endregion // Çözüm 2
+
+            #region Çözüm 3
+            // Klavyeden sayısal veri yerine metinsel veri girildiği zaman uygulama hata verecektir. Bunun önüne geçebilmek için try - catch ile kontrol altına alınarak kullanıcıya bilgi verilecektir.
+
+            // try =>  Hata oluşması muhtemel kod buraya yazılır.
+            // catch => // Hata yakalandığında çalışacak kod buraya yazılır.
+
+            int sayi7, sayi8;
+
+            try
+            {
+                Console.Write("Lütfen sayı1 değerini giriniz: ");
+                sayi7 = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            catch
+            {
+                Console.Write("Lütfen sayı1 değerini giriniz: ");
+                sayi7 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Her iki değer almayı da kontrol ediyoruz.
+
+            try
+            {
+                Console.Write("Lütfen sayı2 değerini giriniz: ");
+                sayi8 = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Write("Lütfen sayı2 değerini giriniz: ");
+                sayi8 = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sayi7 + sayi8);
+            #endregion // Çözüm 3
+            Console.ReadKey(); // Ekranın kapanmaması için yazıldı.
+        }
+    }
+}
---
+++ 02_SoruIki/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_SoruIki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcının girdiği iki sayının karelerinin toplamını veren uygulamayı yazınız?
+
+            // İki tane sayı lazım, kullanıcı girecek
+            // sayıların karelerini al, topla ekrana yazdır.
+
+            #region Çözümler
+            #region Kendi Çözümüm
+            Console.WriteLine("Lütfen birinci sayıyı giriniz: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci sayıyı giriniz: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            int sayi1Kare = sayi1 * sayi1;
+            int sayi2Kare = sayi2 * sayi2;
+
+            Console.WriteLine(sayi1Kare + sayi2Kare);
+            #endregion // Kendi Çözümüm
+
+            #region Çözüm 1
+            Console.WriteLine("Lütfen birinci sayıyı giriniz.");
+            int sayi3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci sayıyı giriniz.");
+            int sayi4 = int.Parse(Console.ReadLine());
+
+            int sonuc = sayi3 * sayi3 + sayi4 * sayi4;
+            Console.WriteLine(sonuc);
+            #endregion // Çözüm 1
+
+            #region Çözüm 2
+            Console.WriteLine("Lütfen birinci sayıyı giriniz.");
+            int sayi5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci sayıyı giriniz.");
+            int sayi6 = Convert.ToInt32(Console.ReadLine());
+
+            // Math sınıfını kullanıyoruz.
+            // Math.Pow(2, 3); 2 nin 3. kuvveti
+
+            double sonuc2 = Math.Pow(sayi5, 2) + Math.Pow(sayi6, 2);
+            // aşağıda double olan dönüş türünü int e cast ettik
+            int sonuc3 = (int)Math.Pow(sayi5, 2) + (int)Math.Pow(sayi6, 2);
+            Console.WriteLine(sonuc3);
+            #endregion // Çözüm 2
+
+            #region Çözüm 3
+            Console.WriteLine("Lütfen birinci ve ikinci sayıları giriniz.");
+            // double sonuc4 = Math.Pow(int.Parse(Console.ReadLine()), 2) + Math.Pow(int.Parse(Console.ReadLine()), 2);
+            Console.WriteLine(Math.Pow(int.Parse(Console.ReadLine()), 2) + Math.Pow(int.Parse(Console.ReadLine()), 2));
+            #endregion // Çözüm 3
+            #endregion // Çözümler
+            Console.Read();
+        }
+    }
+}
